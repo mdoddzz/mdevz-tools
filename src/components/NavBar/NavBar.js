cde0fdb7f80efe0e6e1ef9a157f8086dfe694c05
Mike Dodd <mike@wilxite.com>
2020-09-14 19:28:52 +0100

    Removed deprecated responsive tag

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import React, { Component } from "react";
+import { createMedia } from "@artsy/fresnel";
 import { NavLink, Link } from "react-router-dom";
 import logo from "../../images/mdevz-icon-purple.svg";
 import {
@@ -7,7 +8,7 @@ import {
   Image,
   Menu,
   Sidebar,
-  Responsive,
+  Segment ,
   Dropdown
 } from "semantic-ui-react";
 import './NavBar.css'
@@ -143,6 +144,16 @@ const NavBarChildren = ({ children }) => (
   <div>{children}</div>
 );
 
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 0,
+    computer: 992
+  }
+});
+
+const mediaStyles = AppMedia.createMediaStyle();
+const { Media, MediaContextProvider } = AppMedia;
+
 class ResponsiveNavBar extends Component {
   state = {
     visible: false
@@ -162,21 +173,27 @@ class ResponsiveNavBar extends Component {
 
     return (
       <div>
-        <Responsive {...Responsive.onlyMobile}>
-          <NavBarMobile
-            leftItems={leftItems}
-            onPusherClick={this.handlePusher}
-            onToggle={this.handleToggle}
-            rightItems={rightItems}
-            visible={visible}
-          >
-            <NavBarChildren>{children}</NavBarChildren>
-          </NavBarMobile>
-        </Responsive>
-        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
-          <NavBarDesktop leftItems={leftItems} rightItems={rightItems} />
-          <NavBarChildren>{children}</NavBarChildren>
-        </Responsive>
+
+        <style>{mediaStyles}</style>
+        <MediaContextProvider>
+          <Segment.Group>
+            <Segment as={Media} at="mobile">
+              <NavBarMobile
+                leftItems={leftItems}
+                onPusherClick={this.handlePusher}
+                onToggle={this.handleToggle}
+                rightItems={rightItems}
+                visible={visible}
+              >
+                <NavBarChildren>{children}</NavBarChildren>
+              </NavBarMobile>
+            </Segment>
+            <Segment as={Media} greaterThanOrEqual="computer">
+              <NavBarDesktop leftItems={leftItems} rightItems={rightItems} />
+              <NavBarChildren>{children}</NavBarChildren>
+            </Segment>
+          </Segment.Group>
+        </MediaContextProvider>
       </div>
     );
   }
