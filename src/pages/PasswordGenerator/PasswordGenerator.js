4282ad6dfc65cca95dec2a0f1734a5c6ff8c8ef9
Mike Dodd <mike@wilxite.com>
2020-09-16 11:26:24 +0100

    Updated password generator button

--- src/pages/PasswordGenerator/PasswordGenerator.js
+++ src/pages/PasswordGenerator/PasswordGenerator.js
@@ -169,6 +169,7 @@ export default class PasswordGenerator extends Component {
             <Button 
                 onClick={this.generatePassword}
                 floated='right'
+                primary
                 >
                 Generate Password
             </Button>
