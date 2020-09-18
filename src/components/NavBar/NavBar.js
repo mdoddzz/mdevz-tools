5299b21ebbf0728c53552dc552af550b4a27eff1
Mike Dodd <mike@wilxite.com>
2020-09-18 11:51:49 +0100

    Added dns tracker navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -200,6 +200,7 @@ const leftItems = [
   { as: NavLink, content: "General Tools", to: '/general', key: "security", dropdown: [
     { as: NavLink, content: "Json Formatter", to: '/jsonformatter', key: "jsongormatter" },
     { as: NavLink, content: "URL Shortener", to: '/urlshortener', key: "urlshortener" },
+    { as: NavLink, content: "DNS Checker", to: '/dnschecker', key: "dnschecker" },
   ]},
   { as: NavLink, content: "Security Tools", to: '/security', key: "security", dropdown: [
     { as: NavLink, content: 'Password Generator', to: '/passwordgenerator', key: "passwordgenerator" },
