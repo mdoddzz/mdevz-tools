9eae9ec364a73cb587cecd039ad96b7aeba80b54
Mike Dodd <mike@wilxite.com>
2020-09-20 17:50:54 +0100

    Moved config  to separate file for easy future management

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,16 @@
-import _ from "lodash";
-import React, { Component } from "react";
-import { createMedia } from "@artsy/fresnel";
-import { NavLink, Link } from "react-router-dom";
-import logo from "../../images/mdevz-icon-purple.svg";
+import _ from "lodash"
+import React, { Component } from "react"
+import { createMedia } from "@artsy/fresnel"
+import { NavLink, Link } from "react-router-dom"
+import logo from "../../images/mdevz-icon-purple.svg"
+import { leftItems, rightItems } from '../../config/NavBar'
 import {
   Icon,
   Image,
   Menu,
   Sidebar,
   Dropdown
-} from "semantic-ui-react";
+} from "semantic-ui-react"
 import './NavBar.css'
 
 const NavBarDropdown = ({
@@ -151,23 +152,6 @@ class ResponsiveNavBar extends Component {
     );
   }
 }
-
-const leftItems = [
-  { as: NavLink, content: "General Tools", to: '/general', key: "security", dropdown: [
-    { as: NavLink, content: "Json Formatter", to: '/jsonformatter', key: "jsongormatter" },
-    { as: NavLink, content: "URL Shortener", to: '/urlshortener', key: "urlshortener" },
-    { as: NavLink, content: "DNS Checker", to: '/dnschecker', key: "dnschecker" },
-  ]},
-  { as: NavLink, content: "Security Tools", to: '/security', key: "security", dropdown: [
-    { as: NavLink, content: 'Password Generator', to: '/passwordgenerator', key: "passwordgenerator" },
-    { as: NavLink, content: 'Security Headers', to: '/securityheaders', key: "securityheaders" }
-  ]},
-  { as: NavLink, content: 'Contact', to: '/contact', key: "contact" }
-];
-const rightItems = [
-  //{ as: NavLink, content: "Login", to: '/login', key: "login" },
-  //{ as: NavLink, content: "Register", to: '/register', key: "register" }
-];
 
 export default class NavMenu extends Component {
 
